27de238d63f6e2f0f868a4b942f3edd27310b42a
Eurmthewurm <info@ermoegberts.com>
2026-07-02 08:49:48 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 set -e
+UA="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0 Safari/537.36"
+REF="https://www.authentikstudio.com/"
 mkdir -p dist/assets
 cp index.html dist/index.html
-curl -sL "https://www.authentikstudio.com/ee2%20(1%20of%201).jpg" -o dist/assets/ermo.jpg
-curl -sL "https://www.authentikstudio.com/7c7c58d9-4b00-4c0f-a331-2e74403a1959.jpeg" -o dist/assets/john.jpg
-curl -sL "https://www.authentikstudio.com/bc02b6b2-56a6-4316-b8ed-1586242cc000.jpeg" -o dist/assets/onlocation.jpg
+curl -sSL -A "$UA" -e "$REF" "https://www.authentikstudio.com/ee2%20(1%20of%201).jpg" -o dist/assets/ermo.jpg
+curl -sSL -A "$UA" -e "$REF" "https://www.authentikstudio.com/7c7c58d9-4b00-4c0f-a331-2e74403a1959.jpeg" -o dist/assets/john.jpg
+curl -sSL -A "$UA" -e "$REF" "https://www.authentikstudio.com/bc02b6b2-56a6-4316-b8ed-1586242cc000.jpeg" -o dist/assets/onlocation.jpg
