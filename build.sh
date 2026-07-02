5a20ba781b15a6900ea0aec5065aba72a786f2bc
Eurmthewurm <info@ermoegberts.com>
2026-07-02 15:57:14 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,6 +12,3 @@ cp "john camera 2.jpeg" dist/assets/field2.jpg || true
 cp "john on set.jpeg" dist/assets/field3.jpg || true
 cp "john truck.jpeg" dist/assets/field4.jpg || true
 cp "john back truck.jpeg" dist/assets/field5.jpg || true
-curl -sSL -A "$UA" -e "$REF" "https://www.authentikstudio.com/ee2%20(1%20of%201).jpg" -o dist/assets/ermo.jpg || true
-curl -sSL -A "$UA" -e "$REF" "https://www.authentikstudio.com/7c7c58d9-4b00-4c0f-a331-2e74403a1959.jpeg" -o dist/assets/john.jpg || true
-curl -sSL -A "$UA" -e "$REF" "https://www.authentikstudio.com/bc02b6b2-56a6-4316-b8ed-1586242cc000.jpeg" -o dist/assets/onlocation.jpg || true
